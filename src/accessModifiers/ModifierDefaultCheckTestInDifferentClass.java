87657f6e9a214c7f7de088972fccd7efa6e0db65
JITUN NAYAK <jitunnayak02@gmail.com>
2024-10-18 02:51:20 +0530

    Revert "Interface"
    This reverts commit e4af4ec7bf0bc425d0659f32c44e9d568f52f879.

--- src/accessModifiers/ModifierDefaultCheckTestInDifferentClass.java
+++
@@ -1,9 +0,0 @@
-package accessModifiers;
-
-public class ModifierDefaultCheckTestInDifferentClass {
-    public static void main(String[] args) {
-        ModifierDefault md = new ModifierDefault();
-        System.out.println(md.name);
-        md.display();
-    }
-}
--- src/com/string/StringBufferDemo.java
+++
@@ -1,9 +0,0 @@
-package com.string;
-
-public class StringBufferDemo {
-    public static void main(String[] args) {
-        StringBuffer sb=new StringBuffer("Jitun ");
-        sb.append("Nayak");
-        System.out.println(sb);
-    }
-}
